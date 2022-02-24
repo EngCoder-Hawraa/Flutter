5b414fc5a6a6b115e3babaf7350202fa313d87e9
EngCoder-Hawraa <h.a.j.engineer@gmail.com>
2022-02-24 20:37:04 +0300

    Create the first layout with flutter

---
+++ first_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'layouts/home_screen.dart';
+
+void main() => runApp(MyFlutterApp());
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, //banner above is hide from the page
+        title: "First App",
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Hello Flutter")
+            ),
+            body: HomeScreen()
+        )
+    );
+  }
+}
