e94c0d70c7d3fc7d27259e08f371feff85a0c20d
EngCoder-Hawraa <h.a.j.engineer@gmail.com>
2022-02-24 20:51:26 +0300

    Using container widgets in flutter

---
+++ first_app/lib/layouts/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return
+      Center(
+          child: Container(
+            alignment: Alignment.center,
+            color: Colors.yellow,
+            width: 200,
+            height:200,
+            //   margin: EdgeInsets.all(10),
+            //   margin: EdgeInsets.only(left: 30,top:20),
+            padding: EdgeInsets.all(50),
+            child: Text("Hi Flutter", textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+          )
+      );
+  }
+}
---
+++ first_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'layouts/home_screen.dart';
+
+void main() => runApp(MyFlutterApp());
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  HomeScreen();
+  }
+}
